3129d649fabc5f381b0694dee4368ea2c55cec1a
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2024-04-20 16:18:31 -0500

    dep tweaks, remove useless lib code

--- halogen/src/lib.rs
+++ halogen/src/lib.rs
@@ -3,18 +3,3 @@ pub mod v1;
 
 /// Use the current version's definitions
 pub use v1::*;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
