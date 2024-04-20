b857a8d0874af9ebb4b2628ed1652623da69bf1e
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2024-04-20 00:31:24 -0500

    Put tests back, macro tests and changes

--- halobar_config/src/config_struct.rs
+++ halobar_config/src/config_struct.rs
@@ -1,9 +1,11 @@
 /// toml_edit really doesn't like it when I have required values. This macro is a shitty workaround.
 ///
 /// generates `StructNameConfig` and `StructNameKnown` structs.
-/// `StructNameConfig` is full of `Option<T>`s that get converted to `T`s in `StructNameKnown::hydrate`.
-/// ```rs
+/// `StructNameConfig` is full of `Option<T>`s that get converted to `T`s in `StructNameKnown::overlay`.
+/// ```rust no_run
 /// config_struct! {
+///     @known {Clone}  // extra derives for the known type
+///     @config {Clone, PartialEq}   // extra derives for the config type
 ///     [Window]
 ///     inner_spacing: i32 = 10,
 ///     border_width: u32 = 10,
@@ -20,16 +22,16 @@
 /// ```
 #[macro_export]
 macro_rules! config_struct {
-    ($( @known {$( $known_derive:path ),+} )? [$struct_name:ident] $( $( $( @conf #[$conf_meta:meta] (#[$conf_opt_meta:meta]) )? @conf $conf_key:ident: $conf_mod:path => $conf_name:tt, )* $(@reg #[$meta:meta] (#[$opt_meta:meta]) )? $name:ident: $type:ty = $default:expr ),+$(,)?) => {
+    ($( @known {$( $known_derive:path ),+} )? $( @config {$( $config_derive:path ),+} )? [$struct_name:ident] $( $( $( @conf $( @known #[$conf_known_meta:meta] )* $( @config #[$conf_config_meta:meta] )* )? @conf $conf_key:ident: $conf_mod:path => $conf_name:tt, )* $( @known #[$known_meta:meta] )* $( @config #[$config_meta:meta] )* $name:ident: $type:ty = $default:expr ),+$(,)?) => {
         ::paste::paste! {
             #[derive(Debug, ::serde::Serialize, ::serde::Deserialize $($(, $known_derive )+)?)]
             pub struct [<$struct_name Known>] {
                 $(
                     $(
-                        $( #[$conf_meta] )?
+                        $($( #[$conf_known_meta] )*)?
                         pub $conf_key: $conf_mod::[<$conf_name Known>],
                     )*
-                    $( #[$meta] )?
+                    $( #[$known_meta] )*
                     pub $name: $type,
                 )+
             }
@@ -71,24 +73,63 @@ macro_rules! config_struct {
                 }
             }
 
-            #[derive(Debug, Default, ::serde::Serialize, ::serde::Deserialize)]
+            #[derive(Debug, Default, ::serde::Serialize, ::serde::Deserialize $($(, $config_derive )+)?)]
             pub struct [<$struct_name Config>] {
                 $(
                     $(
-                        $( #[$conf_opt_meta] )?
+                        $($( #[$conf_config_meta] )*)?
                         pub $conf_key: $conf_mod::[<$conf_name Config>],
                     )*
-                    $( #[$opt_meta] )?
+                    $( #[$config_meta] )*
                     pub $name: Option<$type>,
                 )+
             }
 
             impl [<$struct_name Config>] {
                 /// Convert into a Known-type, applying the default values if need be.
-                pub fn hydrate(self) -> [<$struct_name Known>] {
+                pub fn into_known(self) -> [<$struct_name Known>] {
                     [<$struct_name Known>]::overlay(self)
                 }
             }
         }
     };
+}
+
+#[cfg(test)]
+mod test {
+    config_struct! {
+        @known {Clone, Copy, PartialEq, Eq}
+        @config {Clone, PartialEq}
+        [Test]
+        id: u8 = 0,
+    }
+
+    config_struct! {
+        @known {Clone, PartialEq, Eq}
+        [TestNest]
+        @known #[serde(skip)]
+        is_default: bool = true,
+        @conf @config #[serde(flatten)]
+        @conf test: super::test => Test,
+        // due to syntax limitations, each nested config struct must be followed by a regular key-value.
+        is_normal: &'static str = "maybe",
+    }
+
+    #[test]
+    fn config_struct_merge() {
+        let conf = TestNestConfig {
+            is_default: Some(false),
+            test: TestConfig { id: Some(69) },
+            is_normal: None,
+        };
+        let conf_known = conf.into_known();
+
+        let conf_definite = TestNestKnown {
+            is_default: false,
+            test: TestKnown { id: 69 },
+            is_normal: "maybe",
+        };
+
+        assert_eq!(conf_known, conf_definite)
+    }
 }
--- halobar_config/src/lib.rs
+++ halobar_config/src/lib.rs
@@ -1,6 +1,3 @@
-#[cfg(test)]
-mod test;
-
 mod config_struct;
 
 use std::{env, path::PathBuf};
@@ -31,4 +28,21 @@ pub fn xdg_config_home() -> Option<PathBuf> {
     }
 
     None
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    /// This is a fallible test! It relies on environment variables.
+    #[test]
+    fn config_home() {
+        let mut cfg_home = PathBuf::from(env::var_os(HOME).unwrap());
+        for stub in CONFIG_JOIN_STUBS {
+            cfg_home.push(stub);
+        }
+        // safety: This is single-threaded
+        env::set_var(XDG_CONFIG_HOME, &cfg_home);
+
+        assert_eq!(xdg_config_home().unwrap(), cfg_home);
+    }
 }
