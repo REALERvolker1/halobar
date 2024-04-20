7b53549dd516670677c66b61bbaf10c04a2ab800
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2024-04-20 18:35:55 -0500

    Add socket path logic

--- halogen/src/lib.rs
+++ halogen/src/lib.rs
@@ -1,8 +1,88 @@
 /// API version 1
 pub mod v1;
+/// Use the current version's definitions
+pub use v1::*;
 
 /// An internal library for json abstractions
 pub(crate) mod json;
 
-/// Use the current version's definitions
-pub use v1::*;
+use std::{env, path::PathBuf, sync::Arc};
+
+#[cfg(feature = "flume")]
+use flume::unbounded;
+
+#[cfg(feature = "flume")]
+pub type ServerSender<T> = flume::Sender<T>;
+#[cfg(feature = "flume")]
+pub type ServerReceiver<T> = flume::Receiver<T>;
+
+/// Override the path to the socket.
+pub const SOCKET_OVERRIDE_VARIABLE: &str = "HALOGEN_SOCK";
+
+pub struct Server {
+    pub socket_path: PathBuf,
+    /// A Sender to send messages to the socket
+    pub sender: Arc<ServerSender<Message>>,
+    receiver: ServerReceiver<Message>,
+}
+impl Server {
+    pub async fn new() -> Result<Self, Error> {
+        let socket_path = get_socket_path()?;
+        let (s, receiver) = unbounded();
+
+        Ok(Self {
+            socket_path,
+            sender: Arc::new(s),
+            receiver,
+        })
+    }
+}
+
+/// Try to get a valid socket path location
+pub fn get_socket_path() -> Result<PathBuf, Error> {
+    if let Some(env_path) = env::var_os(SOCKET_OVERRIDE_VARIABLE) {
+        let env_path = PathBuf::from(env_path);
+        return if env_path.exists() {
+            Ok(env_path)
+        } else {
+            Err(Error::InvalidSocketPath(env_path))
+        };
+    }
+
+    let mut runtime_dir = match env::var_os("XDG_RUNTIME_DIR") {
+        Some(p) => PathBuf::from(p),
+        None => return Err(Error::InvalidSocketPath(PathBuf::new())),
+    };
+
+    let path_metadata = runtime_dir.metadata()?;
+    if !path_metadata.is_dir() || path_metadata.permissions().readonly() {
+        return Err(Error::InvalidSocketPath(runtime_dir));
+    }
+
+    runtime_dir.push("halogen.sock");
+
+    Ok(runtime_dir)
+}
+
+/// All the errors returned by this crate
+#[derive(Debug)]
+pub enum Error {
+    /// An error wrapping [`std::io::Error`]
+    Io(std::io::Error),
+    /// An error returned by [`get_socket_path`] when the socket path is invalid.
+    InvalidSocketPath(PathBuf),
+}
+impl std::error::Error for Error {}
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Io(e) => e.fmt(f),
+            Self::InvalidSocketPath(p) => write!(f, "Invalid socket path: {}", p.display()),
+        }
+    }
+}
+impl From<std::io::Error> for Error {
+    fn from(value: std::io::Error) -> Self {
+        Self::Io(value)
+    }
+}
