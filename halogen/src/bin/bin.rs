abd273c6df502142d4b4883e9c75a231a402a4bc
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2024-04-21 01:31:13 -0500

    Add bin

---
+++ halogen/src/bin/bin.rs
@@ -0,0 +1,31 @@
+#![cfg_attr(not(feature = "bin"), no_std)]
+#[cfg(not(feature = "bin"))]
+fn main() {}
+
+#[cfg(feature = "bin")]
+fn main() -> Result<(), halogen::Error> {
+    use std::io::IsTerminal;
+
+    let logger = tracing_subscriber::FmtSubscriber::builder()
+        .pretty()
+        .with_ansi(std::io::stdout().is_terminal())
+        .with_writer(|| std::io::stdout());
+    logger.init();
+
+    let future = async move {
+        let mut server = halogen::server::Server::new().await?;
+
+        let server_handle = tokio::spawn(async move { server.read_forever().await });
+
+        // safety: trust me bro
+        server_handle.await.unwrap()?;
+        Ok::<_, halogen::Error>(())
+    };
+
+    tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()?
+        .block_on(future)?;
+
+    Ok(())
+}
