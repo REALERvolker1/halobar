82c6c0adbbb3d8451a8ea8e785f986a4d8bf301d
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2024-05-10 20:52:26 -0500

    Change channel back to mpsc

--- src/types/bichannel.rs
+++ src/types/bichannel.rs
@@ -1,12 +1,12 @@
-use crate::prelude::{error, Arc};
+use crate::prelude::{error, mpsc, Arc};
 
 /// A two-way mpmc channel.
 ///
 /// TODO: Document more
 pub struct BiChannel<T, F> {
     pub context: String,
-    pub sender: Arc<flume::Sender<T>>,
-    pub receiver: Arc<flume::Receiver<F>>,
+    pub sender: Arc<mpsc::Sender<T>>,
+    pub receiver: mpsc::Receiver<F>,
 }
 impl<T, F> BiChannel<T, F> {
     /// Create a new two-way mpsc channel. The buffer is the number of messages it holds before applying backpressure,
@@ -16,8 +16,8 @@ impl<T, F> BiChannel<T, F> {
         first_context: Option<S>,
         second_context: Option<S>,
     ) -> (BiChannel<T, F>, BiChannel<F, T>) {
-        let (sender1, receiver1) = flume::bounded(buffer);
-        let (sender2, receiver2) = flume::bounded(buffer);
+        let (sender1, receiver1) = mpsc::channel(buffer);
+        let (sender2, receiver2) = mpsc::channel(buffer);
 
         (
             BiChannel {
@@ -26,7 +26,7 @@ impl<T, F> BiChannel<T, F> {
                     None => "None".to_owned(),
                 },
                 sender: Arc::new(sender1),
-                receiver: Arc::new(receiver2),
+                receiver: receiver2,
             },
             BiChannel {
                 context: match second_context {
@@ -34,14 +34,15 @@ impl<T, F> BiChannel<T, F> {
                     None => "None".to_owned(),
                 },
                 sender: Arc::new(sender2),
-                receiver: Arc::new(receiver1),
+                receiver: receiver1,
             },
         )
     }
+
     /// Try to send a message through the channel. If it succeeds, this returns true.
     /// If it fails, it logs an error and returns false.
     pub async fn send(&self, item: T) -> bool {
-        match self.sender.send_async(item).await {
+        match self.sender.send(item).await {
             Ok(()) => true,
             Err(e) => {
                 error!(
